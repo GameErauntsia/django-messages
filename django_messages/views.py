da26b07f38e104322c0ffdf193d6c503e2813138
Urtzi Odriozola <uodriozola@codesyntax.com>
2015-03-13 16:15:59 +0100

    import HttpResponse

--- django_messages/views.py
+++ django_messages/views.py
@@ -1,5 +1,6 @@
 import json
 from django.http import Http404, HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.contrib import messages
